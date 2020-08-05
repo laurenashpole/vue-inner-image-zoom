cf3451533a005f70addb2ea33f2e3bf1158795e7
Lauren Ashpole <lauren@laurenashpole.com>
2020-08-04 23:08:24 -0400

    Installed component, added deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:laurenashpole/vue-inner-image-zoom.git gh-pages-source:gh-pages
+
+cd -
