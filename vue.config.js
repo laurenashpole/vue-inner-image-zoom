d37da252fe804935da46b6625f497cf30917bd8a
laurenashpole <lauren@laurenashpole.com>
2021-11-28 23:22:18 -0500

    Upgraded all packages

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,13 @@
+const path = require('path');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-inner-image-zoom/' : '/',
-  chainWebpack: (config) => {
-    config.resolve.alias.set('vue', '@vue/compat');
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      });
+  configureWebpack: {
+    resolve: {
+      symlinks: false,
+      alias: {
+        vue: path.resolve(`./node_modules/vue`)
+      }
+    }
   }
 };
