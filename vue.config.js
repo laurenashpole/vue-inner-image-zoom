6a43ca00380028499bd69d7b9947d6c2f821008c
laurenashpole <lauren@laurenashpole.com>
2021-11-28 19:59:53 -0500

    Upgraded Vue and dependencies, added migration build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-inner-image-zoom/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-inner-image-zoom/' : '/',
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat');
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      });
+  }
 };
