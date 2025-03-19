af8c41d4496f62ffdd909fa393d1809f7fe3465f
Lauren Ashpole <lauren.ashpole@getcatch.com>
2025-03-19 11:59:48 -0500

    deploy

---
+++ js/about.f0ada2b0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var a=t("7a23"),c={class:"about"},o=Object(a["createElementVNode"])("h1",null,"This is an about page",-1),r=[o];function s(e,n){return Object(a["openBlock"])(),Object(a["createElementBlock"])("div",c,r)}var u=t("d959"),b=t.n(u);const i={},l=b()(i,[["render",s]]);n["default"]=l}}]);
+//# sourceMappingURL=about.f0ada2b0.js.map
